43ef5d5e8700443283276679d8fab6f9c73d5230
Yesifu Damilola <yesifudamilolamax@gmail.com>
2024-12-07 03:33:38 +0100

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,6 +8,8 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 const isDevelopment = process.env.NODE_ENV === "development";
+
+console.log("isDevelopment", isDevelopment);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
