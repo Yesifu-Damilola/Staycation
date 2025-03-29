4965f224bebc3a0fa383ad1f2c8b839c9ac81db0
Yesifu-Damilola <yesifudamilolamax@gmail.com>
2025-03-29 10:35:40 +0100

    Added css style to Mostpicked component

--- src/components/features/home/Mostpicked.jsx
+++ src/components/features/home/Mostpicked.jsx
@@ -77,11 +77,11 @@ const Mostpicked = () => {
                 className="absolute top-0 right-0"
               />
               <span
-                className={`text-xl font-bold absolute bottom-2 left-5 py-6 text-[#152C5B] `}
+                className={`text-xl font-bold absolute bottom-2 left-5 py-6 text-white `}
               >
                 {resort.name}
               </span>
-              <p className={`text-base absolute bottom-2 left-5 text-black `}>
+              <p className={`text-base absolute bottom-2 left-5 text-white `}>
                 {resort.location}
               </p>
             </Link>
